14050b20d546c7a66726cb56136764bf34799cb1
Ali Hallaji <ali.hallaji1@gmail.com>
2016-05-20 00:32:49 +0430

    Add criteria into update function 6

--- config/settings_local.py
+++ config/settings_local.py
@@ -21,7 +21,7 @@ YOUTUBE_API_VERSION = "v3"
 period_days = 120
 period_years = 1
 max_page_crawl = 100000
-retry_update_count = 15
+retry_update_count = 40
 ########################################################################
 
 # Debug mode ###########################################################
