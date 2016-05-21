1a67cec96c62a5ac71a90896ee361ce903108016
Ali Hallaji <ali.hallaji1@gmail.com>
2016-05-21 03:04:45 -0400

    asdsad sda

--- services/plugins/crawler/libs/func_tools.py
+++ services/plugins/crawler/libs/func_tools.py
@@ -28,11 +28,7 @@ api_key_update = DEVELOPER_KEY2
 def open_url_api(video_id):
     base_url = "https://www.googleapis.com/youtube/v3/videos?id="
     base_url += video_id
-<<<<<<< HEAD
-    base_url += "&key=" + DEVELOPER_KEY
-=======
     base_url += "&key=" + api_key_update
->>>>>>> 3dccae9c8719d4189d18d13cb3f2f8b0edfc9844
     base_url += "&part=statistics,snippet"
 
     response = urllib.urlopen(base_url).read()
