4dcf9928db2137be3d32fec87551c93f20db1ea3
Ali Hallaji <ali.hallaji1@gmail.com>
2016-06-07 10:36:05 +0430

    Fixed pre final 8

--- services/plugins/crawler/libs/migrate_to_mysql.py
+++ services/plugins/crawler/libs/migrate_to_mysql.py
@@ -40,26 +40,31 @@ def yt_mosted_viewed():
     query = "ALTER DATABASE `newdatabase` CHARACTER SET 'utf8' COLLATE"
     query += " 'utf8_unicode_ci'"
     sql_cursor.execute(query)
+    mydb.commit()
 
     query = "ALTER TABLE songs_chart MODIFY COLUMN YTDESCRIPTION"
-    query += " varchar(500) DEFAULT '' CHARACTER SET 'utf8'"
+    query += " varchar(500) DEFAULT '' CHARACTER SET utf8"
     query += " COLLATE utf8_general_ci;"
     sql_cursor.execute(query)
+    mydb.commit()
 
     query = "ALTER TABLE songs_chart MODIFY COLUMN YTTitle"
-    query += " varchar(500) DEFAULT '' CHARACTER SET 'utf8'"
-    query += " COLLATE utf8_general_ci;"
+    query += " varchar(500) DEFAULT '' CHARACTER SET utf8"
+    query += " COLLATE utf8_general_ci NOT NULL;"
     sql_cursor.execute(query)
+    mydb.commit()
 
     query = "ALTER TABLE songs_chart MODIFY COLUMN Song"
-    query += " varchar(500) DEFAULT '' CHARACTER SET 'utf8'"
-    query += " COLLATE utf8_general_ci;"
+    query += " varchar(500) DEFAULT '' CHARACTER SET utf8"
+    query += " COLLATE utf8_general_ci NOT NULL;"
     sql_cursor.execute(query)
+    mydb.commit()
 
     query = "ALTER TABLE songs_chart MODIFY COLUMN Artist"
-    query += " varchar(500) DEFAULT '' CHARACTER SET 'utf8'"
-    query += " COLLATE utf8_general_ci;"
+    query += " varchar(500) DEFAULT '' CHARACTER SET utf8"
+    query += " COLLATE utf8_general_ci NOT NULL;"
     sql_cursor.execute(query)
+    mydb.commit()
 
     _date = datetime.datetime.now().replace(hour=4, minute=30)
     # last_date = _date - datetime.timedelta(days=1)
